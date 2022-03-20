2bdcac7771f6e0421a6314652aeebce17499457f
Grabarz88 <pawel.otto1999@o2.pl>
2022-03-20 03:38:52 +0100

    Update EnemyFightingScript.cs
    Enemy's movement works just the way it should. Fighting mechanism itself isn't yet added.

--- Assets/EnemyFightingScript.cs
+++ Assets/EnemyFightingScript.cs
@@ -8,6 +8,8 @@ public class EnemyFightingScript : MonoBehaviour
     public GameObject target; //target is player in this case
     public Vector2 targetDir; //This Vector is pointing at the player
     public Vector2 targetMovementDir; //The enemy will try to mirror player's movement. This Vector will be taken straight from player's object;
+    public float velocityInTargetDir;
+    
     Rigidbody2D rb;
     
     void Start() {
@@ -31,8 +33,24 @@ public class EnemyFightingScript : MonoBehaviour
         targetDir = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
         targetDir = targetDir.normalized;
         
+        
         targetMovementDir = target.gameObject.GetComponent<PlayerMovement>().getMirroredMovement();
-        rb.velocity = (targetDir * 100) + targetMovementDir;
+
+        velocityInTargetDir = Vector2.Dot(targetMovementDir, targetDir); 
+        
+        // if(velocityInTargetDir<0f)
+        // {
+        // rb.velocity = (targetDir * 100) + (targetMovementDir/2) - (targetDir * velocityInTargetDir);
+        // Debug.Log("Daleko");
+        // }
+        // else if(velocityInTargetDir>=0f)
+        // {
+        // rb.velocity = (targetDir * 100) + (targetMovementDir/2);
+        // Debug.Log("Blisko");
+        // }
+
+        rb.velocity = (targetDir * 100) + ((targetMovementDir-(targetDir * velocityInTargetDir))/2);
+        
 
        
         
