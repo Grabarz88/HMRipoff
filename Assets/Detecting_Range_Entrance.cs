f1b9aabb37bea15346d8a39b332a13368d2792c8
Grabarz88 <pawel.otto1999@o2.pl>
2022-04-07 21:43:56 +0200

    Added Enemy Punching animation and it's activator
    The actual effects of punch are yet to be added. Basicly system of punching and punch cooldown in Punch Range Detector should be moved to Enemy Fighting Script and improved

---
+++ Assets/Detecting_Range_Entrance.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detecting_Range_Entrance : MonoBehaviour
+{
+public bool inRange = false;
+public Animator detective_animator;
+
+
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {   
+    if(other.gameObject.CompareTag("Player")){  
+    detective_animator.SetBool("Punching", true);
+    StartCoroutine (punchingAnimation());    
+
+    }
+
+
+    IEnumerator punchingAnimation()
+    {
+       yield return new WaitForSeconds(0.2f);
+       detective_animator.SetBool("Punching", false); 
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
